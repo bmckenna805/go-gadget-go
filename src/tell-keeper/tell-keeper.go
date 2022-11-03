e1711686d3455320016c2238c902b68cb9a2ea8e
Benjamin McKenna <benjamin.mckenna@dotdashmdp.com>
2022-11-03 10:31:49 -0500

    Add finalized tell-keeper code here

--- src/tell-keeper/tell-keeper.go
+++ src/tell-keeper/tell-keeper.go
@@ -23,33 +23,33 @@ func getClient(token string, configLoc string) *ksm.SecretsManager {
 	return ksm.NewSecretsManager(clientOptions)
 }
 
-func createRecord(name string) *ksm.Record {
-  record := ksm.NewRecordCreate(name, "created by tell-keeper")
+func createRecord(name string) *ksm.RecordCreate {
+  record := ksm.NewRecordCreate(name, name)
   return record
 }
 
-func appendLogin(record *ksm.Record, login string) *ksm.Record{
-  record.Fields = append(customLogin.Fields,
+func appendLogin(record *ksm.RecordCreate, login string) *ksm.RecordCreate {
+  record.Fields = append(record.Fields,
           ksm.NewLogin(login),
   )
   return record
 }
 
-func appendPassword(record *ksm.Record, password string) *ksm.Record{
-  record.Fields = append(customLogin.Fields,
+func appendPassword(record *ksm.RecordCreate, password string) *ksm.RecordCreate {
+  record.Fields = append(record.Fields,
           ksm.NewPassword(password),
   )
   return record
 }
 
-func createSecret(folder string, record *ksm.Record,  client *ksm.SecretsManager) []interface{} {
-  recordUid, err := sm.CreateSecretWithRecordData("", folder, record)
+func createSecret(folder string, record *ksm.RecordCreate,  client *ksm.SecretsManager) string {
+  recordUid, err := client.CreateSecretWithRecordData("", folder, record)
   if err != nil {
         log.Fatal(err)
     } else {
         return recordUid
     }
-    return nil
+    return ""
 }
 
 func main() {
@@ -77,12 +77,12 @@ func main() {
     if *namePtr != "" {
         record := createRecord(*namePtr)
         if *passwordPtr != "" {
-            appendPassword(*record, *passwordPtr)
+            appendPassword(record, *passwordPtr)
         }
         if *loginPtr != "" {
-            appendLogin(*record, *loginPtr)
+            appendLogin(record, *loginPtr)
         }
-        secretUid := createSecret(*folderPtr, *record, client)
+        secretUid := createSecret(*folderPtr, record, client)
         fmt.Println(secretUid)
     }
 }
